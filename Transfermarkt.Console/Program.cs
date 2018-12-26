23eebfeb29e43db11d4789c7fbf89763d23fa4b3
Leonel Bagorro <Leonel.Bagorro@vision-box.com>
2018-12-26 17:25:48 +0000

    Console App created

---
+++ Transfermarkt.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transfermarkt.Core;
+using Transfermarkt.Core.Connectors;
+using Transfermarkt.Core.Contracts;
+
+namespace Transfermarkt.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConnection conn = new TransfermarktConnection(new HtmlAgilityPackConnector());
+
+            string url = "https://www.transfermarkt.pt/fc-barcelona/kader/verein/131/saison_id/2018/plus/1";
+            conn.ParseSquad(url);
+        }
+    }
+}
