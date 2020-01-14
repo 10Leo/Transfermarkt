5f6c5eb60bbdefcb55356754ba76ce780ade4a9e
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-01-14 00:09:12 +0000

    Refactored Nationality Converter to read it's options from a file.

--- Transfermarkt.Core/Converters/Nationality/PTNationalityConverter.cs
+++ Transfermarkt.Core/Converters/Nationality/PTNationalityConverter.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,22 +14,50 @@ namespace Transfermarkt.Core.Converters
 {
     public class PTNationalityConverter : INationalityConverter
     {
-        public Nationality? Convert(string stringToConvert)
+        private static readonly string language = "PT";
+        private static readonly string dateFormat = "yyyy-MM-dd";
+
+        public static string SettingsFolderPath { get; } = ConfigurationManager.AppSettings["SettingsFolderPath"].ToString();
+        public static string SettingsNationalitiesFile { get; } = ConfigurationManager.AppSettings["SettingsNationalitiesFile"].ToString();
+
+        private readonly IDictionary<string, Actors.Nationality> map = new Dictionary<string, Actors.Nationality>();
+
+        public PTNationalityConverter()
         {
-            return Converter(stringToConvert);
+            JsonSerializerSettings settings = new JsonSerializerSettings
+            {
+                DateFormatString = dateFormat,
+            };
+            settings.Formatting = Formatting.Indented;
+            settings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+
+            string json = File.ReadAllText($@"{SettingsFolderPath}\{language}\{SettingsNationalitiesFile}");
+
+            var definition = new { Language = default(string), Set = new[] { new { Name = default(string), DO = default(string) } } };
+            var deserializedJSON = JsonConvert.DeserializeAnonymousType(json, definition);
+            deserializedJSON.Set.ToList().ForEach(p =>
+            {
+                Enum.TryParse(p.DO, out Actors.Nationality toDomainObject);
+                map.Add(p.Name, toDomainObject);
+            });
         }
 
-        private Nationality? Converter(string str)
+        public Nationality? Convert(string stringToConvert)
         {
-            switch (str)
+            Actors.Nationality? p = null;
+            try
             {
-                case "Brasil": return Nationality.BRA;
-                case "Inglaterra": return Nationality.GBR;
-                case "Itália": return Nationality.ITA;
-                case "Espanha": return Nationality.SPA;
-                case "Portugal": return Nationality.PRT;
-                default: return null;
+                p = map[stringToConvert];
             }
+            catch (KeyNotFoundException)
+            {
+                //log
+            }
+            catch (ArgumentNullException)
+            {
+                //log
+            }
+            return p;
         }
     }
 }
