72822a15f1c064eb1bb291dc984a2d5d996c2586
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-01-18 20:40:10 +0000

    Domain redesigned.

---
+++ Transfermarkt.Core/Contracts/IMarketValueParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transfermarkt.Core.Contracts
+{
+    public interface IMarketValueParser<TNode> : IElementParser<TNode, string>
+    {
+    }
+}
