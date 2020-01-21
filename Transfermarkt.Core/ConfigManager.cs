ba498f7783298e0f4ef5fa841c0dc867698e8d0e
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-01-21 22:55:40 +0000

    Config Manager logic created.

---
+++ Transfermarkt.Core/ConfigManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transfermarkt.Core.Contracts;
+
+namespace Transfermarkt.Core
+{
+    public class ConfigManager : IConfigurationManager
+    {
+        public string GetAppSetting(string key)
+        {
+            return ConfigurationManager.AppSettings["SettingsFolderPath"].ToString();
+        }
+    }
+}
---
+++ Transfermarkt.Core/Contracts/IConfigurationManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transfermarkt.Core.Contracts
+{
+    public interface IConfigurationManager
+    {
+        string GetAppSetting(string key);
+    }
+}
