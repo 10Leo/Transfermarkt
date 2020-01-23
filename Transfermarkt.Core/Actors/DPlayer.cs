ec34399be7c9cf9facec8fca2b056da7a1cb1ffe
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-01-23 02:01:25 +0000

    Created the notion of IElement, which are containers for IDomain attributes, ie, values parsed from the pages.

---
+++ Transfermarkt.Core/Actors/DPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transfermarkt.Core.Actors
+{
+    class DPlayer : ID
+    {
+        public IList<IElement> Properties { get; set; }
+
+        public DPlayer()
+        {
+            Properties.Add(new PlayerName());
+            Properties.Add(new Height());
+        }
+    }
+
+    public class PlayerName : IElement
+    {
+        public string Name { get; set; }
+        public dynamic Value { get; set; }
+    }
+
+    public class Height : IElement
+    {
+        public string Name { get; set; }
+        public dynamic Value { get; set; }
+    }
+
+    public interface ID
+    {
+        IList<IElement> Properties { get; set; }
+
+    }
+
+    public interface IElement
+    {
+        string Name { get; set; }
+        dynamic Value { get; set; }
+    }
+}
