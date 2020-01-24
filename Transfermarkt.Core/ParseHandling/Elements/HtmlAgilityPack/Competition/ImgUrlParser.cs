2a9de9e46001bc8888d5a7faaf5934a56910ec7d
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-01-24 01:16:16 +0000

    Created utility class for HtmlAgilityPack.

--- Transfermarkt.Core/ParseHandling/Elements/HtmlAgilityPack/Competition/ImgUrlParser.cs
+++ Transfermarkt.Core/ParseHandling/Elements/HtmlAgilityPack/Competition/ImgUrlParser.cs
@@ -45,30 +45,4 @@ namespace Transfermarkt.Core.ParseHandling.Elements.HtmlAgilityPack.Competition
             return parsedObj;
         }
     }
-
-    static class HtmlAgilityPackUtil
-    {
-        public static T GetAttributeValue<T>(this HtmlNode td, string key, T defaultValue) where T : IConvertible
-        {
-            T result = defaultValue;//default(T);
-
-            if (td.Attributes[key] == null)// || String.IsNullOrEmpty(td.Attributes[key].Value) == false)
-            {
-                return defaultValue;
-            }
-
-            string value = td.Attributes[key].Value;
-
-            try
-            {
-                result = (T)Convert.ChangeType(value, typeof(T));
-            }
-            catch
-            {
-                result = defaultValue;//default(T);
-            }
-
-            return result;
-        }
-    }
 }
