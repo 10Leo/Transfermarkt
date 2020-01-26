a6848b8371d1c5bb4eddcd9134b852fadf777c69
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-01-26 21:18:16 +0000

    Club and Player now implement abstract Domain instead of it's interface.

--- Transfermarkt.Core/Actors/Club.cs
+++ Transfermarkt.Core/Actors/Club.cs
@@ -1,16 +1,11 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Transfermarkt.Core.ParseHandling.Contracts;
 using Transfermarkt.Core.ParseHandling.Elements.Club;
 
 namespace Transfermarkt.Core.Actors
 {
-    public class Club : IDomain
+    public class Club : Domain
     {
-        public IList<IElement> Elements { get; set; }
-
-        public IList<IDomain> Children { get; set; }
-
         public Club()
         {
             Elements = new List<IElement>
@@ -21,49 +16,6 @@ namespace Transfermarkt.Core.Actors
                 new ImgUrl(),
                 new CountryImg()
             };
-
-            Children = new List<IDomain>();
-        }
-
-        public IElement SetElement(IElement element)
-        {
-            if (element == null)
-            {
-                return null;
-            }
-            var elementType = element.GetType();
-
-            //var thisElement = Elements.FirstOrDefault(p => p.GetType() == elementType);
-            //thisElement.Value = element.Value;
-
-            foreach (var e in Elements)
-            {
-                if (e.GetType() == elementType)
-                {
-                    e.Value = element.Value;
-                    return e;
-                }
-            }
-
-            return null;
-        }
-
-        public override string ToString()
-        {
-            return string.Format("{0}\n{1}"
-                , string.Join(
-                    ", "
-                    , Elements.Select(
-                        p => p.ToString()
-                    )
-                )
-                , string.Join(
-                    ", "
-                    , Children.Select(
-                        p => p.ToString()
-                    )
-                )
-            );
         }
     }
 }
--- Transfermarkt.Core/Actors/Player.cs
+++ Transfermarkt.Core/Actors/Player.cs
@@ -1,15 +1,9 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Transfermarkt.Core.ParseHandling.Contracts;
-using Transfermarkt.Core.ParseHandling.Elements.Player;
-
 namespace Transfermarkt.Core.Actors
 {
-    class Player : IDomain
+    class Player : Domain
     {
-        public IList<IElement> Elements { get; set; }
-        public IList<IDomain> Children { get; set; } = null;
-
         public Player()
         {
             Elements = new List<IElement>
@@ -33,46 +27,6 @@ namespace Transfermarkt.Core.Actors
                 new Transfermarkt.Core.ParseHandling.Elements.Player.ImgUrl(),
                 new Transfermarkt.Core.ParseHandling.Elements.Player.ProfileUrl(),
             };
-
-            Children = new List<IDomain>();
-        }
-
-        public IElement SetElement(IElement element)
-        {
-            if (element == null)
-            {
-                return null;
-            }
-            var elementType = element.GetType();
-
-            foreach (var e in Elements)
-            {
-                if (e.GetType() == elementType)
-                {
-                    e.Value = element.Value;
-                    return e;
-                }
-            }
-
-            return null;
-        }
-
-        public override string ToString()
-        {
-            return string.Format("{0}\n{1}"
-                , string.Join(
-                    ", "
-                    , Children.Select(
-                        p => p.ToString()
-                    )
-                )
-                , string.Join(
-                    ", "
-                    , Elements.Select(
-                        p => p.ToString()
-                    )
-                )
-            );
         }
     }
 }
