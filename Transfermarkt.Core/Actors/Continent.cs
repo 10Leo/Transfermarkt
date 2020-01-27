39c904f0c00b6fc6236203d8475498c1958a6048
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-01-27 00:11:12 +0000

    Added Continent Domain.

--- Transfermarkt.Core/Actors/Continent.cs
+++ Transfermarkt.Core/Actors/Continent.cs
@@ -1,32 +1,17 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Transfermarkt.Core.ParseHandling.Contracts;
 
 namespace Transfermarkt.Core.Actors
 {
-    public class Continent// : IDomain
+    public class Continent : Domain
     {
-        public string Name { get; set; }
-        public ContinentCode? ContinentCode { get; set; }
-        public IList<Competition> Competitions { get; set; }
-
         public Continent()
         {
-            Competitions = new List<Competition>();
-        }
-
-        public override string ToString()
-        {
-            return string.Format("{0} [{1}]\n{2}"
-                , Name
-                , ContinentCode
-                , string.Join(
-                    "\n"
-                    , Competitions.Select(
-                        p => p.ToString()
-                    )
-                )
-            );
+            Elements = new List<IElement>
+            {
+                new Transfermarkt.Core.ParseHandling.Elements.Continent.Name(),
+                new Transfermarkt.Core.ParseHandling.Elements.Continent.ContinentCode()
+            };
         }
     }
 }
