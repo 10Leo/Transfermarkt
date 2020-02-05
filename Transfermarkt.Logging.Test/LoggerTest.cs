fac6dae00ce44398b9aea436e28cc023748f20c2
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-02-05 21:57:59 +0000

    Created a test project for logging.

---
+++ Transfermarkt.Logging.Test/LoggerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Transfermarkt.Logging.Test
+{
+    [TestClass]
+    public class LoggerTest
+    {
+        [TestMethod]
+        public void LogsAnException()
+        {
+            var logger = LoggerFactory.GetLogger();
+            for (int i = 0; i < 10; i++)
+            {
+                logger.LogException(new Exception("this is supposed to be an exception"));
+            }
+        }
+
+        [TestMethod]
+        public void LogsAMessage()
+        {
+            var logger = LoggerFactory.GetLogger();
+            for (int i = 0; i < 10; i++)
+            {
+                logger.WriteMessage("this is a message from the future me");
+            }
+        }
+    }
+}
---
+++ Transfermarkt.Logging.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Transfermarkt.Logging.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Transfermarkt.Logging.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9cc5ca04-871a-4db6-a9a7-4d330160f1a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
