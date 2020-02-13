1112ac2dece45d910d2a19617dd49a963493303f
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-02-13 21:37:32 +0000

    Added IValue.

---
+++ Transfermarkt.Core/ParseHandling/Contracts/IValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transfermarkt.Core.ParseHandling.Contracts
+{
+    public interface IValue<T>
+    {
+        T Value { get; set; }
+    }
+
+    public interface StringValue: IValue<string> { }
+    public interface IntValue: IValue<int> { }
+}
