52a9185fab07d70a22a48bf87686d8824c48dd0e
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-03-01 16:30:56 +0000

    Converters domain specific contracts moved to different zone of the project.

--- Transfermarkt.Core/ParseHandling/Contracts/Converter/IFootConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿using Transfermarkt.Core.Actors;
-
-namespace Transfermarkt.Core.ParseHandling.Contracts.Converter
-{
-    // TODO: These sub IConverters need to be replaced outside of the contracts, as they are domain specific (these ones belong to transfermarkt, they aren't need in a different domain like the fifa one for the site scraper)
-    public interface IFootConverter : IConverter<FootValue>
-    {
-    }
-}
