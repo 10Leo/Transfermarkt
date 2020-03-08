e4eb194b372076e05e16cb8b3f8cdaf0b8096ee6
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-03-08 16:20:48 +0000

    HAPConnection refactored to allow parsing of local html files.

--- Transfermarkt.Core/ParseHandling/HAPConnection.cs
+++ Transfermarkt.Core/ParseHandling/HAPConnection.cs
@@ -17,15 +17,16 @@ namespace Transfermarkt.Core.ParseHandling
         {
             try
             {
-                string htmlCode = "";
+                string htmlString = "";
                 using (WebClient client = new WebClient())
                 {
+                    Uri uri = new Uri(url);
                     client.Encoding = System.Text.Encoding.GetEncoding("UTF-8");
                     client.Headers.Add(HttpRequestHeader.UserAgent, "AvoidError");
-                    htmlCode = client.DownloadString(url);
+                    htmlString = client.DownloadString(uri);
                 }
                 doc = new HtmlAgilityPack.HtmlDocument();
-                doc.LoadHtml(htmlCode);
+                doc.LoadHtml(htmlString);
             }
             catch (System.Net.WebException ex)
             {
