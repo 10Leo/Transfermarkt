950ebec0b1c5cd452c982edea9fa058e80c795d1
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-04-04 22:53:47 +0100

    App refactored to insert logs via templating.

--- LogsReader/logsReader-app.js
+++ LogsReader/logsReader-app.js
@@ -1,6 +1,8 @@
-(function() {
+var LogReader = LogReader || {};
 
-	this.LogEntry = function(entry) {
+window.LogReader.app = (function() {
+
+	this.LogReader.LogEntry = function(entry) {
 		this.date = entry.date;
 		this.type = entry.type;
 		this.msg = entry.msg;
@@ -9,113 +11,101 @@
 		this.ex = entry.ex;
 	}
 
-    // Define our constructor
-    this.Log = function() {
-		this.logFile = GetLogFile('\log_20200328.txt');
-    }
+	this.LogReader.LogEntry.prototype.CreateUI = function() {
+		var temp = document.getElementById("logEntry-template");
+        var clone = temp.content.cloneNode(true);
 
-    Log.prototype.format = function() {
-		var html = [];
-		var node = document.createElement("tr");
-		
-		node.innerHTML = html.join('');
-        return node;
+		if(this.date !== undefined){
+			clone.querySelector(".logEntry__date").textContent = this.date;
+		}
+		if(this.type !== undefined){
+			clone.querySelector(".logEntry__type").textContent = this.type;
+		}
+		if(this.msg !== undefined){
+			clone.querySelector(".logEntry__msg").textContent = this.msg;
+		}
+		if(this.innerText !== undefined){
+			clone.querySelector(".logEntry__innerText").textContent = this.innerText;
+		}
+		if(this.innerHtml !== undefined){
+			clone.querySelector(".logEntry__innerHtml").innerHtml = this.innerHtml;
+		}
+		if(this.ex !== undefined){
+			clone.querySelector(".logEntry__ex").textContent = this.ex;
+		}
+
+        return clone;
     }
 	
-	function appendLog(parentNode, logEntry){
-		var node = document.createElement("tr");
 
-		var logDate = document.createElement("td");
-		var logType = document.createElement("td");
-		var logMsg = document.createElement("td");
-		var logInnerText = document.createElement("td");
-		var logInnerHtml = document.createElement("td");
-		var logEx = document.createElement("td");
+	var app = {
+		logFile: 'log_20200328.txt',
+        logs: [],
 
-		if(logEntry.date !== undefined){
-			logDate.innerHTML = logEntry.date;
-		}
-		if(logEntry.type !== undefined){
-			logType.innerHTML = logEntry.type;
-		}
-		if(logEntry.msg !== undefined){
-			logMsg.innerHTML = logEntry.msg;
-		}
-		if(logEntry.innerText !== undefined){
-			logInnerText.innerHTML = logEntry.innerText;
-		}
-		if(logEntry.innerHtml !== undefined){
-			logInnerHtml.innerHTML = logEntry.innerHtml;
-		}
-		if(logEntry.ex !== undefined){
-			logEx.innerHTML = logEntry.ex;
-		}
+        initialize: function () {
+            this.bindEvents();
+        },
 
-		node.appendChild(logDate);
-		node.appendChild(logType);
-		node.appendChild(logMsg);
-		node.appendChild(logInnerText);
-		node.appendChild(logInnerHtml);
-		node.appendChild(logEx);
+        bindEvents: function () {
+            if (window.location.pathname.indexOf('/logsReader.html') !== -1) {
+                window.addEventListener('load', this.onDeviceReady.bind(this), false);
+            }
+        },
 
-		parentNode.appendChild(node);
-	}
+        onDeviceReady: function () {
+            this.getLogs()
+                .then(response => response.text())
+                .then(app.parseLogs)
+                .then(app.renderData);
+		},
+		
+        getLogs: function () {
+            return fetch(this.logFile);
+		},
+		
+        parseLogs: function (data) {
+			var entryLogs = data.match(/(?<=\[\[)([^\[][^\[]|[^\]][^\]]*)(?=\s*\]\])/g);
+			entryLogs.forEach(entryLog => {
+				var logParts = entryLog.match(/(?<=\<\|\|)([^\<][^\|][^\|]|[^\|][^\|][^\>]*)(?=\s*\|\|\>)/g);
 
-	function GetLogFile(file){
-		this.file = fetch(file)
-			.then(response => response.text())
-			.then((data) => {
-
-				var parentNode = document.getElementById('tabLogs').querySelector("tbody");
-				if (typeof(parentNode) === "undefined" && some_variable === null) {
-					return;
+				var log = {};
+				if(logParts.length > 0){
+					log.date = logParts[0];
 				}
-
-				var entryLogs = data.match(/(?<=\[\[)([^\[][^\[]|[^\]][^\]]*)(?=\s*\]\])/g);
-				entryLogs.forEach(entryLog => {
-					var logParts = entryLog.match(/(?<=\<\|\|)([^\<][^\|][^\|]|[^\|][^\|][^\>]*)(?=\s*\|\|\>)/g);
-
-					var log = {};
-					if(logParts.length > 0){
-						log.date = logParts[0];
-					}
-					if(logParts.length > 1){
-						log.type = logParts[1];
-					}
-					if(logParts.length > 2){
-						log.msg = logParts[2];
-					}
-					if(logParts.length > 3){
-						log.innerText = logParts[3];
-					}
-					if(logParts.length > 4){
-						log.innerHtml = logParts[4];
-					}
-					if(logParts.length > 5){
-						log.ex = logParts[5];
-					}
-					
-					var logEntry = new LogEntry(log);
-					appendLog(parentNode, logEntry);
-				});
+				if(logParts.length > 1){
+					log.type = logParts[1];
+				}
+				if(logParts.length > 2){
+					log.msg = logParts[2];
+				}
+				if(logParts.length > 3){
+					log.innerText = logParts[3];
+				}
+				if(logParts.length > 4){
+					log.innerHtml = logParts[4];
+				}
+				if(logParts.length > 5){
+					log.ex = logParts[5];
+				}
 				
-				return data;
-			})
-	}
+				app.logs.push(
+					new LogReader.LogEntry(log)
+				);
+			});
+			
+			return data;
+		},
+		
+        renderData: function () {
+            var basePane = document.getElementById('logs-list');
+            app.logs.forEach(n => {
+                var thisLogs = n.CreateUI();
+                basePane.appendChild(thisLogs);
+            });
+        }
+	};
+	
+    return app;
 }());
 
-function init() {
-	var d = document.getElementById('content');
-
-	var t = new Log();
-
-	var f = t.file;
-	// for (var i = 0; i < todo.list.length; i++) {
-	// 	var t = new Log(todo.list[i]);
-	// 	d.appendChild(t.format());
-	// }
-}
-
-$(document).ready(function() {
-	init();
-});
+LogReader.app.initialize();
