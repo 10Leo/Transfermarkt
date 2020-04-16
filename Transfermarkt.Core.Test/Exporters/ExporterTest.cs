8664835f1dd917c341f678b2073ac138869d1d7d
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-04-16 22:28:59 +0100

    Added unit test to the export as JSON functionality.

---
+++ Transfermarkt.Core.Test/Exporters/ExporterTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Transfermarkt.Core.Actors;
+using Transfermarkt.Core.Exporter;
+using Transfermarkt.Core.ParseHandling.Contracts;
+using Transfermarkt.Exporter.JSONExporter;
+
+namespace Transfermarkt.Core.Test.Exporters
+{
+    [TestClass]
+    public class ExporterTest
+    {
+        [TestMethod]
+        public void SuccessfullyExportsDomainsAsJSON()
+        {
+            IExporter exporter = new JsonExporter();
+            IDomain<IValue> domain = GenerateClub(Nationality.PRT, "Benfica", 2020, "http://benfica.pt", "http://benfica.pt");
+
+            IDomain<IValue> player01 = GeneratePlayer("Nemanja Matic", "Matic", new DateTime(1985, 1, 1), Nationality.SRB, 191, Foot.L, Position.CM, 6, 0, new DateTime(2012, 1, 1), new DateTime(2016, 7, 1), 600000000, "http://link", "http://link");
+            IDomain<IValue> player02 = GeneratePlayer("Lio Messi", "Messi", new DateTime(1986, 1, 1), Nationality.ARG, 171, Foot.L, Position.AM, 10, 1, new DateTime(2003, 7, 1), new DateTime(2022, 7, 1), 200000000, "http://link", "http://link");
+            IDomain<IValue> player03 = GeneratePlayer("Jonas", "Jonas", new DateTime(1981, 1, 1), Nationality.BRA, 182, Foot.R, Position.SS, 10, 0, new DateTime(2015, 7, 1), new DateTime(2020, 7, 1), 20000000, "http://link", "http://link");
+
+            domain.Children.Add(player01);
+            domain.Children.Add(player02);
+            domain.Children.Add(player03);
+            
+            exporter.Extract(domain);
+        }
+
+        private Club GenerateClub(Nationality nationality, string name, int season, string imgUrl, string countryImg)
+        {
+            Club domain = new Club();
+
+            ((Core.ParseHandling.Elements.Club.Country)domain.Elements.FirstOrDefault(e => e.InternalName == "Country")).Value = new NationalityValue { Value = nationality };
+            ((Core.ParseHandling.Elements.Club.Name)domain.Elements.FirstOrDefault(e => e.InternalName == "Name")).Value = new StringValue { Value = name };
+            ((Core.ParseHandling.Elements.Club.Season)domain.Elements.FirstOrDefault(e => e.InternalName == "Y")).Value = new IntValue { Value = season };
+            ((Core.ParseHandling.Elements.Club.ImgUrl)domain.Elements.FirstOrDefault(e => e.InternalName == "ImgUrl")).Value = new StringValue { Value = imgUrl };
+            ((Core.ParseHandling.Elements.Club.CountryImg)domain.Elements.FirstOrDefault(e => e.InternalName == "CountryImg")).Value = new StringValue { Value = countryImg };
+
+            return domain;
+        }
+
+        private Player GeneratePlayer(string name, string shortName, DateTime birthDate, Nationality nat, int h, Foot foot, Position pos, int number, int cap, DateTime clubArrivalDate, DateTime contractExpirationDate, decimal mv, string imgUrl, string profileUrl)
+        {
+            Player domain = new Player();
+
+            ((Core.ParseHandling.Elements.Player.BirthDate)domain.Elements.FirstOrDefault(e => e.InternalName == "BirthDate")).Value = new DatetimeValue { Value = birthDate };
+            ((Core.ParseHandling.Elements.Player.Captain)domain.Elements.FirstOrDefault(e => e.InternalName == "Cap")).Value = new IntValue { Value = cap };
+            ((Core.ParseHandling.Elements.Player.ClubArrivalDate)domain.Elements.FirstOrDefault(e => e.InternalName == "ClubArrivalDate")).Value = new DatetimeValue { Value = clubArrivalDate };
+            ((Core.ParseHandling.Elements.Player.ContractExpirationDate)domain.Elements.FirstOrDefault(e => e.InternalName == "ContractExpirationDate")).Value = new DatetimeValue { Value = contractExpirationDate };
+            ((Core.ParseHandling.Elements.Player.Height)domain.Elements.FirstOrDefault(e => e.InternalName == "H")).Value = new IntValue { Value = h };
+            ((Core.ParseHandling.Elements.Player.ImgUrl)domain.Elements.FirstOrDefault(e => e.InternalName == "ImgUrl")).Value = new StringValue { Value = imgUrl };
+            ((Core.ParseHandling.Elements.Player.MarketValue)domain.Elements.FirstOrDefault(e => e.InternalName == "MV")).Value = new DecimalValue { Value = mv };
+            ((Core.ParseHandling.Elements.Player.Name)domain.Elements.FirstOrDefault(e => e.InternalName == "Name")).Value = new StringValue { Value = name };
+            ((Core.ParseHandling.Elements.Player.Nationality)domain.Elements.FirstOrDefault(e => e.InternalName == "Nat")).Value = new NationalityValue { Value = nat };
+            ((Core.ParseHandling.Elements.Player.Position)domain.Elements.FirstOrDefault(e => e.InternalName == "Pos")).Value = new PositionValue { Value = pos };
+            ((Core.ParseHandling.Elements.Player.PreferredFoot)domain.Elements.FirstOrDefault(e => e.InternalName == "Foot")).Value = new FootValue { Value = foot };
+            ((Core.ParseHandling.Elements.Player.ProfileUrl)domain.Elements.FirstOrDefault(e => e.InternalName == "ProfileUrl")).Value = new StringValue { Value = profileUrl };
+            ((Core.ParseHandling.Elements.Player.ShirtNumber)domain.Elements.FirstOrDefault(e => e.InternalName == "#")).Value = new IntValue { Value = number };
+            ((Core.ParseHandling.Elements.Player.ShortName)domain.Elements.FirstOrDefault(e => e.InternalName == "ShortName")).Value = new StringValue { Value = shortName };
+
+            return domain;
+        }
+    }
+}
