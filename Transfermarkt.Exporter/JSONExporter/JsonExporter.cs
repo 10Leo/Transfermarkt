58ede8a3ce9eafb11a4008a9e5ec99fe5387caf7
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-04-17 20:32:42 +0100

    Invalid chars are now escaped in the filename.

--- Transfermarkt.Exporter/JSONExporter/JsonExporter.cs
+++ Transfermarkt.Exporter/JSONExporter/JsonExporter.cs
@@ -182,7 +182,17 @@ namespace Transfermarkt.Exporter.JSONExporter
                 fileName = fileName.Replace($"{{{k}}}", value);
             }
 
+            fileName = MakeValidFileName(fileName) + format;
+
             return fileName;
+        }
+
+        private static string MakeValidFileName(string name)
+        {
+            string invalidChars = System.Text.RegularExpressions.Regex.Escape(new string(System.IO.Path.GetInvalidFileNameChars()));
+            string invalidRegStr = string.Format(@"([{0}]*\.+$)|([{0}]+)", invalidChars);
+
+            return System.Text.RegularExpressions.Regex.Replace(name, invalidRegStr, "_");
         }
 
         private static string CreateBaseDir()
