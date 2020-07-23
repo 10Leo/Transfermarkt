f63a950985af89f4a25fbda51921aa84e0cb5b31
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-07-24 00:01:30 +0100

    DateTime values only print the date part.

--- Page.Scraper/Values/DatetimeValue.cs
+++ Page.Scraper/Values/DatetimeValue.cs
@@ -8,12 +8,14 @@ namespace Page.Scraper.Contracts
 {
     public class DatetimeValue : IValue
     {
+        private readonly string dateFormat = "dd/MM/yyyy";
+
         public Type Type => typeof(DateTime?);
         public DateTime? Value { get; set; }
 
         public override string ToString()
         {
-            return string.Format("{0}", Value);
+            return string.Format("{0}", Value?.ToString(dateFormat));
         }
     }
 }
