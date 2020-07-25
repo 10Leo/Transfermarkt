6ccb51e61cdc68bd2096320992570144311afb0f
Leonel Bagorro <leonelbagorro.10@gmail.com>
2020-07-25 16:44:36 +0100

    Page partial parsing test made more robust.

--- Transfermarkt.Core.Test/Pages/PageTest.cs
+++ Transfermarkt.Core.Test/Pages/PageTest.cs
@@ -6,6 +6,8 @@ using Transfermarkt.Logging;
 using System.Linq;
 using System.Collections.Generic;
 using Page.Scraper.Contracts;
+using Transfermarkt.Core.ParseHandling.Parsers.HtmlAgilityPack.Continent;
+using Transfermarkt.Core.ParseHandling.Elements.Continent;
 
 namespace Transfermarkt.Core.Test.ParseHandling.Pages
 {
@@ -17,6 +19,18 @@ namespace Transfermarkt.Core.Test.ParseHandling.Pages
 
         private static readonly ILogger logger = LoggerFactory.GetLogger((LogLevel)MinimumLoggingLevel);
 
+        private readonly string ptComp = "Portugal-Liga NOS", engComp = "Inglaterra-Premier League", spaComp = "Espanha-LaLiga", itaComp = "Itália-Serie A";
+
+        private readonly string append = "/wettbewerbe?plus=1";
+        private readonly IDictionary<Actors.ContinentCode, string> urls = new Dictionary<Actors.ContinentCode, string>
+        {
+            { Actors.ContinentCode.EEE, "https://www.transfermarkt.pt/wettbewerbe/europa" },
+            { Actors.ContinentCode.SRR, "https://www.transfermarkt.pt/wettbewerbe/amerika" },
+            { Actors.ContinentCode.NNN, "https://www.transfermarkt.pt/wettbewerbe/amerika" },
+            { Actors.ContinentCode.ABB, "https://www.transfermarkt.pt/wettbewerbe/asien" },
+            { Actors.ContinentCode.FFF, "https://www.transfermarkt.pt/wettbewerbe/afrika" }
+        };
+
         [TestMethod, TestCategory("Page")]
         public void TestPageMethods()
         {
@@ -27,63 +41,6 @@ namespace Transfermarkt.Core.Test.ParseHandling.Pages
             Assert.IsTrue(section.Name == "Club - Players Section", "The returned Section was different than the one expected.");
         }
 
-        [TestMethod, TestCategory("Page")]
-        public void TestPartialParsing()
-        {
-            //page.Parse(url, new Link[] { new Link { ID = "1.1.1", Title = "" } });
-            string[] urls = new string[]
-            {
-                "https://www.transfermarkt.pt/wettbewerbe/europa",
-                "https://www.transfermarkt.pt/wettbewerbe/amerika",
-                "https://www.transfermarkt.pt/wettbewerbe/asien",
-                "https://www.transfermarkt.pt/wettbewerbe/afrika"
-            };
-            var append = "/wettbewerbe?plus=1";
-
-            ContinentPage continentPage = new ContinentPage(new HAPConnection(), logger, 2009);
-
-            //TODO: consider passing url in constructor making it a required param and as a result, always available to the functions.
-            continentPage.Connect(urls[0]);
-
-            var sectionsToParse = new List<ISection>
-            {
-                continentPage["Continent Details"]
-            };
-
-            continentPage.Parse(sectionsToParse);
-
-            //sectionsToParse = new List<ISection>
-            //{
-            //    continentPage["Continent - Competitions Section"]
-            //};
-            //continentPage.Parse(sectionsToParse, false);
-
-
-            var childSection = (ChildsSection<HtmlAgilityPack.HtmlNode, CompetitionPage>)continentPage["Continent - Competitions Section"];
-            Assert.IsNotNull(childSection, "The returned Section is null.");
-            Assert.IsTrue(childSection.Name == "Continent - Competitions Section", "The returned Section was different than the one expected.");
-
-            childSection.Parse(false);
-
-
-            var childrenToParse = childSection.Children.Where(u => u.Title == "Portugal-Liga NOS"
-                                                                || u.Title == "Inglaterra-Premier League"
-                                                                || u.Title == "Espanha-LaLiga"
-                                                                || u.Title == "Itália-Serie A"
-            );
-            childSection.Parse(childrenToParse);
-
-            var compPagePT = childSection["Portugal-Liga NOS"];
-            Assert.IsNotNull(compPagePT, "The returned Page is null.");
-
-            compPagePT.Parse(parseChildren: true);
-
-
-            var domain = continentPage.Domain;
-            Assert.IsNotNull(domain, "The returned Domain is null.");
-            //Assert.IsTrue(domain.Children.Count == (4 - 1), "There should be only 3 records as one of the 4 supplied links was a repetition and no repetition should be parsed.");
-        }
-
         [TestMethod, TestCategory("Page Parsing")]
         public void TestClubParsing()
         {
@@ -91,7 +48,7 @@ namespace Transfermarkt.Core.Test.ParseHandling.Pages
 
             ClubPage page = new ClubPage();
             page.Connect(url);
-            page.Parse();
+            page.Parse(parseChildren: true);
 
             var domain = page.Domain;
             Assert.IsNotNull(domain, "The returned Domain is null.");
@@ -110,7 +67,7 @@ namespace Transfermarkt.Core.Test.ParseHandling.Pages
             url = "https://www.transfermarkt.pt/liga-nos/startseite/wettbewerb/PO1/plus/?saison_id=2009";
             CompetitionPage page = new CompetitionPage();
             page.Connect(url);
-            page.Parse();
+            page.Parse(parseChildren: true);
 
             var domain = page.Domain;
             Assert.IsNotNull(domain, "The returned Domain is null.");
@@ -134,22 +91,78 @@ namespace Transfermarkt.Core.Test.ParseHandling.Pages
         [TestMethod, TestCategory("Page Parsing")]
         public void TestContinentParsing()
         {
-            string[] urls = new string[]
+            foreach (KeyValuePair<Actors.ContinentCode, string> url in urls)
             {
-                "https://www.transfermarkt.pt/wettbewerbe/europa",
-                "https://www.transfermarkt.pt/wettbewerbe/amerika",
-                "https://www.transfermarkt.pt/wettbewerbe/asien",
-                "https://www.transfermarkt.pt/wettbewerbe/afrika"
-            };
-            var append = "/wettbewerbe?plus=1";
-
-            ContinentPage page = null;
-            foreach (var url in urls)
-            {
-                page = new ContinentPage(new HAPConnection(), logger, null);
-                page.Connect(url);
+                ContinentPage page = new ContinentPage(new HAPConnection(), logger, null);
+                page.Connect(url.Value);
                 page.Parse();
             }
+        }
+
+        [TestMethod, TestCategory("Page Parsing")]
+        public void TestPartialParsing()
+        {
+            ContinentPage continentPage = new ContinentPage(new HAPConnection(), logger, 2009);
+
+            //TODO: consider passing url in constructor making it a required param and as a result, always available to the functions.
+            continentPage.Connect(urls[Actors.ContinentCode.EEE]);
+
+            var sectionsToParse = new List<ISection> { continentPage["Continent Details"] };
+            continentPage.Parse(sectionsToParse);
+
+            Assert.IsTrue(((ContinentCode)continentPage.Domain.Elements.FirstOrDefault(e => e.InternalName == "Code")).Value.Value == Actors.ContinentCode.EEE);
+            Assert.IsTrue(continentPage.Domain.Children.Count == 0, "No children should exist yet as no ChildSection was passed to be parsed.");
+
+            //sectionsToParse = new List<ISection> { continentPage["Continent - Competitions Section"] };
+            //continentPage.Parse(sectionsToParse, false);
+
+            var childSection = (ChildsSection<HtmlAgilityPack.HtmlNode, CompetitionPage>)continentPage["Continent - Competitions Section"];
+            Assert.IsNotNull(childSection, "The returned Section is null.");
+            Assert.IsTrue(childSection.Name == "Continent - Competitions Section", "The returned Section was different than the one expected.");
+
+            childSection.Parse(false);
+            Assert.IsTrue(childSection.Children.Count > 0, "Children Links should have been fetched.");
+            Assert.IsTrue(continentPage.Domain.Children.Count == 0, "No domain children should exist yet as the param parseChildren was set to false.");
+
+            IList<string> linksToParse = new List<string> { ptComp, engComp, spaComp, itaComp };
+            IEnumerable<string> nonExistingLinksToParse = new List<string> { "Non-Existant League 01", "Non-Existant League 02" };
+
+            var childrenToParse = childSection.Children.Where(u => linksToParse.Contains(u.Title) || nonExistingLinksToParse.Contains(u.Title));
+            childSection.Parse(childrenToParse);
+            Assert.IsTrue(continentPage.Domain.Children.Count == linksToParse.Count(), $"There should exist {linksToParse.Count} children.");
+
+            var ctp = linksToParse.Select(l => l.Split('-')?[1]);
+            for (int i = 0; i < continentPage.Domain.Children.Count; i++)
+            {
+                var childCompetition = continentPage.Domain.Children[i];
+
+                Assert.IsTrue(ctp.Contains(((Core.ParseHandling.Elements.Competition.Name)childCompetition.Elements.FirstOrDefault(e => e.InternalName == "Name")).Value.Value), "Parsed a different competition children than the one expected.");
+            }
+
+
+            foreach (string pageName in linksToParse)
+            {
+                IPage<IDomain, HtmlAgilityPack.HtmlNode> compPage = childSection[pageName];
+                Assert.IsNotNull(compPage, $"The returned Page {pageName} is null.");
+                Assert.IsTrue(compPage.Domain.Children.Count == 0, $"No Club children should exist for {pageName}.");
+            }
+
+
+            IPage<IDomain, HtmlAgilityPack.HtmlNode> compPagePT = childSection[ptComp];
+            Assert.IsNotNull(compPagePT, $"The returned Page {ptComp} is null.");
+
+            compPagePT.Parse(parseChildren: true);
+            Assert.IsTrue(compPagePT.Domain.Children.Count > 0, $"There should exist club children on {ptComp}.");
+            foreach (string pageName in linksToParse.Where(l => l != ptComp))
+            {
+                IPage<IDomain, HtmlAgilityPack.HtmlNode> compPage = childSection[pageName];
+                Assert.IsNotNull(compPage, $"The returned Page {pageName} is null.");
+                Assert.IsTrue(compPage.Domain.Children.Count == 0, $"No Club children should exist for {pageName}.");
+            }
+
+
+            var domain = continentPage.Domain;
+            Assert.IsNotNull(domain, "The returned Domain is null.");
         }
     }
 }
