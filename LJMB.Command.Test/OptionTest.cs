511e4151205efe19f3a93a5ac542de37efb6354e
Leonel Bagorro <leonelbagorro.10@gmail.com>
2021-01-10 02:24:15 +0000

    Additional OptionTest class created.

---
+++ LJMB.Command.Test/OptionTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using LJMB.Command.Commands;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LJMB.Command.Test
+{
+    [TestClass]
+    public class OptionTest
+    {
+        private readonly List<IOption> options = new List<IOption>();
+        
+        [TestInitialize]
+        public void Initialize()
+        {
+            options.Add(new MockOption());
+        }
+
+
+    }
+}
